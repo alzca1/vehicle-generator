68d0429804fbf35a852a7cd68ab0d0ca3b488957
Álvaro Zájara <alzca1@gmail.com>
2019-11-24 18:54:20 +0100

    working second form

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,12 @@
 //Selección elementos DOM
+var carform = document.querySelector(".carform");
+var wheelform = document.querySelector(".wheelform");
 var carDetails = document.querySelectorAll(".carDetails");
-var carBrand = document.querySelectorAll(".brand");
-var submitButton = document.querySelector("button");
+var carWBrand = document.querySelectorAll(".wbrand");
+var carWDiameter = document.querySelectorAll(".diameter");
+var submitButton = document.querySelector("#submitCarFormButton");
+var submitWheelForm = document.querySelector("#submitWheelFormButton");
 var displayDiv = document.getElementsByClassName("carDisplay");
-console.log(carBrand);
 // Variable Array con características coche
 var carData = [];
 var carWheelBrand = [];
@@ -12,29 +15,38 @@ var carWheelDiam = [];
 var isValidPlate = false;
 var isValidBrand = false;
 var isValidColor = false;
+var isValidWBrand = false;
+var isValidWheel = false;
 // Ejecución eventos (listeners de validación y click)
-function fireEvents() {
+function fireCarForm() {
     carDetails.forEach(function (element) {
-        element.addEventListener("change", validator);
+        element.addEventListener("keyup", validator);
     });
     submitButton.addEventListener("click", function () {
         createCar(carData);
+        // se desactivan los listeners del carForm
+        carDetails.forEach(function (element) {
+            element.removeEventListener("change", validator);
+        });
+        // una vez se crea el coche se activa el wheelForm
+        fireWheelForm();
     });
-    // carBrand.forEach(element => {
-    //   element.addEventListener("click", validator);
-    // });
 }
-// recolección de datos de formulario inicial
-// y envío a variable array carData
-function collectData() {
-    carDetails.forEach(function (element) {
-        var index = element.className.replace(/\D/g, "");
-        carData[index] = element.value;
+function fireWheelForm() {
+    carform.classList.toggle("hidden");
+    wheelform.classList.toggle("hidden");
+    // se añaden los listeners para marca ruedas y diametros
+    carWBrand.forEach(function (element) {
+        element.addEventListener("keyup", wheelValidator);
     });
-    carBrand.forEach(function (element) {
+    carWDiameter.forEach(function (element) {
+        element.addEventListener("change", wheelValidator);
+    });
+}
+function collectData(classGroup, array) {
+    classGroup.forEach(function (element) {
         var index = element.className.replace(/\D/g, "");
-        carWheelBrand[index] = element.value;
-        console.log(carWheelBrand);
+        array[index] = element.value;
     });
 }
 //validador con switch de campos
@@ -54,17 +66,41 @@ function validator(event) {
     //Activador botón submit si validadores son todos true;
     if (isValidPlate && isValidBrand && isValidColor) {
         submitButton.disabled = false;
-        collectData();
+        collectData(carDetails, carData);
     }
     else {
         submitButton.disabled = true;
+    }
+}
+function checkLength(element) {
+    return element.length >= 3;
+}
+function checkValue(element) {
+    console.log(element);
+    if (element >= 0.4) {
+        if (element <= 2) {
+            return element;
+        }
+    }
+}
+function wheelValidator(event) {
+    collectData(carWBrand, carWheelBrand);
+    collectData(carWDiameter, carWheelDiam);
+    isValidBrand = carWheelBrand.every(checkLength);
+    isValidWheel = carWheelDiam.every(checkValue);
+    if (isValidBrand && isValidWheel) {
+        submitWheelForm.disabled = false;
+        collectData(carDetails, carData);
+    }
+    else {
+        submitWheelForm.disabled = true;
     }
 }
 // Creación de objeto coche y envío al DOM
 function createCar(array) {
     var car = new Car(carData[0], carData[1], carData[2]);
     // car.addWheel(new Wheel(2, "SEAT"));
-    var presentation = "#######CAR####### - PLATE: " + car.plate + " -- COLOR: " + car.color + " -- WHEELS: " + JSON.stringify(car.wheels);
+    var presentation = "#######CAR####### - PLATE: " + car.plate + " --BRAND: " + car.brand + " -- COLOR: " + car.color + " -- WHEELS: " + JSON.stringify(car.wheels);
     displayDiv[0].textContent = presentation;
 }
-document.addEventListener("DOMContentLoaded", fireEvents);
+document.addEventListener("DOMContentLoaded", fireCarForm);
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,12 @@
 //Selección elementos DOM
+const carform = document.querySelector(".carform");
+const wheelform = document.querySelector(".wheelform");
 const carDetails = document.querySelectorAll(".carDetails");
-const carBrand = document.querySelectorAll(".brand");
-const submitButton = document.querySelector("button");
+const carWBrand = document.querySelectorAll(".wbrand");
+const carWDiameter = document.querySelectorAll(".diameter");
+const submitButton = document.querySelector("#submitCarFormButton");
+const submitWheelForm = document.querySelector("#submitWheelFormButton")
 const displayDiv = document.getElementsByClassName("carDisplay");
-console.log(carBrand)
 
 // Variable Array con características coche
 let carData: string[] = [];
@@ -14,32 +17,42 @@ let carWheelDiam: string[] = [];
 let isValidPlate: boolean = false;
 let isValidBrand: boolean = false;
 let isValidColor: boolean = false;
-
+let isValidWBrand: boolean = false;
+let isValidWheel: boolean = false;
 // Ejecución eventos (listeners de validación y click)
-function fireEvents() {
+
+function fireCarForm() {
   carDetails.forEach(element => {
-    element.addEventListener("change", validator);
+    element.addEventListener("keyup", validator);
   });
   submitButton.addEventListener("click", function() {
     createCar(carData);
+    // se desactivan los listeners del carForm
+    carDetails.forEach(element => {
+      element.removeEventListener("change", validator);
+    });
+    // una vez se crea el coche se activa el wheelForm
+    fireWheelForm();
   });
-  // carBrand.forEach(element => {
-  //   element.addEventListener("click", validator);
-  // });
 }
 
-// recolección de datos de formulario inicial
-// y envío a variable array carData
-function collectData(): void {
-  carDetails.forEach(element => {
-    var index = element.className.replace(/\D/g, "");
-    carData[index] = element.value;
-  });
+function fireWheelForm() {
+  carform.classList.toggle("hidden");
+  wheelform.classList.toggle("hidden");
 
-  carBrand.forEach(element => {
+  // se añaden los listeners para marca ruedas y diametros
+  carWBrand.forEach(element => {
+    element.addEventListener("keyup", wheelValidator);
+  });
+  carWDiameter.forEach(element => {
+    element.addEventListener("change", wheelValidator)
+  })
+}
+
+function collectData(classGroup, array) {
+  classGroup.forEach(element => {
     var index = element.className.replace(/\D/g, "");
-    carWheelBrand[index] = element.value;
-    console.log(carWheelBrand)
+    array[index] = element.value;
   });
 }
 
@@ -62,9 +75,36 @@ function validator(event: any) {
 
   if (isValidPlate && isValidBrand && isValidColor) {
     submitButton.disabled = false;
-    collectData();
+    collectData(carDetails, carData);
   } else {
     submitButton.disabled = true;
+  }
+}
+
+function checkLength(element) {
+  return element.length >= 3;
+}
+
+function checkValue(element) {
+  console.log(element);
+  if (element >= 0.4) {
+    if (element <= 2) {
+      return element;
+    }
+  }
+}
+
+function wheelValidator(event: any) {
+  collectData(carWBrand, carWheelBrand);
+  collectData(carWDiameter, carWheelDiam);
+  isValidBrand = carWheelBrand.every(checkLength);
+  isValidWheel = carWheelDiam.every(checkValue);
+
+  if (isValidBrand && isValidWheel) {
+    submitWheelForm.disabled = false;
+    collectData(carDetails, carData);
+  } else {
+    submitWheelForm.disabled = true;
   }
 }
 
@@ -73,10 +113,10 @@ function createCar(array: string[]) {
   let car = new Car(carData[0], carData[1], carData[2]);
 
   // car.addWheel(new Wheel(2, "SEAT"));
-  let presentation = `#######CAR####### - PLATE: ${car.plate} -- COLOR: ${
-    car.color
-  } -- WHEELS: ${JSON.stringify(car.wheels)}`;
+  let presentation = `#######CAR####### - PLATE: ${car.plate} --BRAND: ${
+    car.brand
+  } -- COLOR: ${car.color} -- WHEELS: ${JSON.stringify(car.wheels)}`;
   displayDiv[0].textContent = presentation;
 }
 
-document.addEventListener("DOMContentLoaded", fireEvents);
+document.addEventListener("DOMContentLoaded", fireCarForm);
