927753b4acdb68d549ee6d405b56604c1b0c5d01
Álvaro Zájara <alzca1@gmail.com>
2019-11-24 08:14:00 +0100

    first form working

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,70 @@
-"use strict";
-function createCar(plate, brand, color) {
-    var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+//Selección elementos DOM
+var carDetails = document.querySelectorAll(".carDetails");
+var carBrand = document.querySelectorAll(".brand");
+var submitButton = document.querySelector("button");
+var displayDiv = document.getElementsByClassName("carDisplay");
+console.log(carBrand);
+// Variable Array con características coche
+var carData = [];
+var carWheelBrand = [];
+var carWheelDiam = [];
+// Variables booleanas validador características coche
+var isValidPlate = false;
+var isValidBrand = false;
+var isValidColor = false;
+// Ejecución eventos (listeners de validación y click)
+function fireEvents() {
+    carDetails.forEach(function (element) {
+        element.addEventListener("change", validator);
+    });
+    submitButton.addEventListener("click", function () {
+        createCar(carData);
+    });
+    carBrand.forEach(function (element) {
+        element.addEventListener("click", validator);
+    });
 }
+// recolección de datos de formulario inicial
+// y envío a variable array carData
+function collectData() {
+    carDetails.forEach(function (element) {
+        var index = element.className.replace(/\D/g, "");
+        carData[index] = element.value;
+    });
+    carBrand.forEach(function (element) {
+        var index = element.className.replace(/\D/g, "");
+        carWheelBrand[index] = element.value;
+        console.log(carWheelBrand);
+    });
+}
+//validador con switch de campos
+// ###TAREA###: validación matrículas!!!!
+function validator(event) {
+    switch (event.target.id) {
+        case "plate":
+            isValidPlate = event.target.checkValidity();
+            break;
+        case "brand":
+            isValidBrand = event.target.checkValidity();
+            break;
+        case "color":
+            isValidColor = event.target.checkValidity();
+            break;
+    }
+    //Activador botón submit si validadores son todos true;
+    if (isValidPlate && isValidBrand && isValidColor) {
+        submitButton.disabled = false;
+        collectData();
+    }
+    else {
+        submitButton.disabled = true;
+    }
+}
+// Creación de objeto coche y envío al DOM
+function createCar(array) {
+    var car = new Car(carData[0], carData[1], carData[2]);
+    // car.addWheel(new Wheel(2, "SEAT"));
+    var presentation = "#######CAR####### - PLATE: " + car.plate + " -- COLOR: " + car.color + " -- WHEELS: " + JSON.stringify(car.wheels);
+    displayDiv[0].textContent = presentation;
+}
+document.addEventListener("DOMContentLoaded", fireEvents);
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,82 @@
+//Selección elementos DOM
+const carDetails = document.querySelectorAll(".carDetails");
+const carBrand = document.querySelectorAll(".brand");
+const submitButton = document.querySelector("button");
+const displayDiv = document.getElementsByClassName("carDisplay");
+console.log(carBrand)
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+// Variable Array con características coche
+let carData: string[] = [];
+let carWheelBrand: string[] = [];
+let carWheelDiam: string[] = [];
+
+// Variables booleanas validador características coche
+let isValidPlate: boolean = false;
+let isValidBrand: boolean = false;
+let isValidColor: boolean = false;
+
+// Ejecución eventos (listeners de validación y click)
+function fireEvents() {
+  carDetails.forEach(element => {
+    element.addEventListener("change", validator);
+  });
+  submitButton.addEventListener("click", function() {
+    createCar(carData);
+  });
+  carBrand.forEach(element => {
+    element.addEventListener("click", validator);
+  });
 }
 
+// recolección de datos de formulario inicial
+// y envío a variable array carData
+function collectData(): void {
+  carDetails.forEach(element => {
+    var index = element.className.replace(/\D/g, "");
+    carData[index] = element.value;
+  });
+
+  carBrand.forEach(element => {
+    var index = element.className.replace(/\D/g, "");
+    carWheelBrand[index] = element.value;
+    console.log(carWheelBrand)
+  });
+}
+
+//validador con switch de campos
+// ###TAREA###: validación matrículas!!!!
+function validator(event: any) {
+  switch (event.target.id) {
+    case "plate":
+      isValidPlate = event.target.checkValidity();
+      break;
+    case "brand":
+      isValidBrand = event.target.checkValidity();
+      break;
+    case "color":
+      isValidColor = event.target.checkValidity();
+      break;
+  }
+
+  //Activador botón submit si validadores son todos true;
+
+  if (isValidPlate && isValidBrand && isValidColor) {
+    submitButton.disabled = false;
+    collectData();
+  } else {
+    submitButton.disabled = true;
+  }
+}
+
+// Creación de objeto coche y envío al DOM
+function createCar(array: string[]) {
+  let car = new Car(carData[0], carData[1], carData[2]);
+
+  // car.addWheel(new Wheel(2, "SEAT"));
+  let presentation = `#######CAR####### - PLATE: ${car.plate} -- COLOR: ${
+    car.color
+  } -- WHEELS: ${JSON.stringify(car.wheels)}`;
+  displayDiv[0].textContent = presentation;
+}
+
+document.addEventListener("DOMContentLoaded", fireEvents);
